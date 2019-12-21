abc993df71021481c0fb6f52ba1ce2adf094ca65
Mengleifeng <fmleing@163.com>
2019-12-21 14:46:56 +0800

    集合Set、Map并发不安全例子

---
+++ src/main/ContainerNotSafeMapDemo.java
@@ -0,0 +1,7 @@
+/**
+ * 集合Map线程不安全类
+ * HashMap使用CurrentHashMap
+ */
+
+public class ContainerNotSafeMapDemo {
+}
---
+++ src/main/ContainerSetNotSafeDemo.java
@@ -0,0 +1,8 @@
+/**
+ * 集合类set并发不安全例子
+ *
+ * HashSet底层是HashMap，HashSet.add ==> HashMap.put(key,常量就是object)
+ */
+
+public class ContainerSetNotSafeDemo {
+}
